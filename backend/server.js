4a9c07c0b61a01b22a2b3157f833ee039046a9a4
Abhishekkj35 <abhishekkj6810@gmail.com>
2026-01-13 07:02:51 +0530

    added electronic health record

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 const authRoutes = require('./routes/authRoutes');
 const appointmentRoutes = require('./routes/appointmentRoutes');
 const consultationRoutes = require('./routes/consultationRoutes');
+const ehrRoutes = require('./routes/ehrRoutes');
 
 const app = express();
 
@@ -26,6 +27,7 @@ app.get('/', (req, res) => {
 app.use('/api/auth', authRoutes);
 app.use('/api/appointments', appointmentRoutes);
 app.use('/api/consultations', consultationRoutes);
+app.use('/api/ehr', ehrRoutes);
 
 // Start Server
 const PORT = process.env.PORT || 5000;
